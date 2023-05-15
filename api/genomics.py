67e2a027346eb9ffaa5aa3ecc0b02485d928ab52
fossilfriend <egreenfest@gmail.com>
2023-05-15 17:46:27 -0400

    placeholders

--- api/genomics.py
+++ api/genomics.py
@@ -1,1 +1,13 @@
 # pagination
+from flask import Flask, Blueprint
+from flask_apispec import use_kwargs, marshal_with
+
+from marshmallow import fields, Schema
+
+blueprint = Blueprint("genomics_api", "genomics")
+
+@blueprint.route('/genomics/variant')
+@use_kwargs({'id': fields.Str(), 'size': fields.Str()})
+@marshal_with(VariantSchema(many=True))
+def get_pets(**kwargs):
+    return [{"name": "Scooby", "category": "dog", "size": "large"}]
