a0647e88c0f81306d4ddd7df70bdc2def30ff0c4
fossilfriend <egreenfest@gmail.com>
2023-06-12 09:54:26 -0400

    function for extracting value from jsonb  field

---
+++ shared_resources/utils.py
@@ -0,0 +1,13 @@
+def extract_json_value(attribute, field):
+    """extract value from field in a JSON attribute
+
+    Args:
+        attribute (dict): JSON/B attribute
+        field (string): field name
+    
+    """
+    
+    if attribute is None:
+        return None
+    
+    return attribute[field] if field in attribute else None
