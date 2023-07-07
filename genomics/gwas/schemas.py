eb566e39513a3cd4c4900910b122c81597191d57
fossilfriend <egreenfest@gmail.com>
2023-07-06 23:18:32 -0400

    remove track from schema

--- genomics/gwas/schemas.py
+++ genomics/gwas/schemas.py
@@ -1,10 +1,10 @@
 from flask_restx import fields
     
 gwas_result = {
-    'track': fields.String(description="unique identifier of track being queried", required=True),
+    # 'track': fields.String(description="unique identifier of track being queried", required=True),
     'variant_id': fields.String(description="variant identifier", attribute="metaseq_id", required=True),
     'ref_snp_id': fields.String(description="dbSNP refSnpID"),
-    'pvalue': fields.String(description="pvalue, may be in scientific notation", required=True),
+    'pvalue': fields.String(description="pvalue, may be in scientific notation", attribute="pvalue_display", required=True),
     'neg_log10_pvalue': fields.Float(description="-log10(pvalue)", required=True),
     'test_allele': fields.String(description="test allele in the GWAS analysis")
 }
