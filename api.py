ea2891d8e338682b06e578448b48839eebf6399a
fossilfriend <egreenfest@gmail.com>
2023-07-10 01:11:07 -0400

    more home page

--- api.py
+++ api.py
@@ -1,8 +1,7 @@
-from flask_restx import Api
+from flask_restx import Api, swagger
 from config.utils import get_version
 from genomics import genomics_api, genomics_api_children
 from filer import filer_api, filer_api_children
-
 
 version = get_version()
 
@@ -10,7 +9,7 @@ api = Api(
     title='NIAGADS API',
     version=version,
     description='API for accessing NIAGADS knowledgebases',
-    doc='/doc/'
+    doc='/doc/',
 )
 
 api.add_namespace(genomics_api)
