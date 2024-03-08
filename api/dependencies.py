6af85a4189dcf8e4dd57f6c5c63ee41b297b3a69
fossilfriend <egreenfest@gmail.com>
2024-03-08 12:54:57 -0500

    chromosome param

--- api/dependencies.py
+++ api/dependencies.py
@@ -1,5 +1,16 @@
 from fastapi import Query
+from fastapi.exception_handlers import (
+    http_exception_handler,
+    request_validation_exception_handler,
+)
+from fastapi.exceptions import RequestValidationError
+from starlette.exceptions import HTTPException as StarletteHTTPException
+
 from enum import Enum
+
+from niagads.reference.chromosomes import Human as Chromosome
+
+# https://fastapi.tiangolo.com/tutorial/query-params-str-validations/#query-parameter-list-multiple-values
 
 RESPONSES = {404: {"description": "Not found"}}
 class Assembly(str, Enum):
@@ -20,19 +31,7 @@ class Assembly(str, Enum):
 def assembly_param(assembly: Assembly = Query(Assembly.GRCh38, description="reference genome build (assembly)")): 
     return Assembly.validate(assembly)
 
-# https://fastapi.tiangolo.com/tutorial/query-params-str-validations/#query-parameter-list-multiple-values
-__CHR_NS = [str(x) for x in [*range(1,22)]] + ['X', 'Y', 'M']
-__CHR_IDS = [ 'chr' + str(x) for x in __CHR_NS]
-# __CHROMOSOMES = [*__CHR_NS, *__CHR_IDS]
-Chromosome: Enum = Enum('Chromosome', { c.upper() : c for c in __CHR_IDS}, type=str)
-def validate_chromosome(chrom: str | int):
-    strC = str(chrom).upper().replace('CHR', 'chr').replace('MT', 'M')
-    if strC in __CHR_IDS:
-        return strC
-    if strC in __CHR_NS:
-        return 'chr' + strC
-    return None
 
-def chromosome_param(chrom = Query(Chromosome.CHR1, description="chromosome specified as")):
-    return validate_chromosome(chrom)
-    
+def chromosome_param(chromosome: str = Query(Chromosome.chr19.value, enum=[c.name for c in Chromosome],
+        description="chromosome, specificed as 1..22,X,Y,M,MT or chr1...chr22,chrX,chrY,chrM,chrMT")):
+    return Chromosome.validate(chromosome)
