8ee9b7945d8758a9b68b1f85c3bdd70eba320851
fossilfriend <egreenfest@gmail.com>
2024-04-04 13:17:23 -0400

    remove rewrites

--- next.config.js
+++ next.config.js
@@ -3,34 +3,8 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-
     distDir: 'build',
         
-    rewrites: async () => {
-        return [
-            {
-                source: "/api/:path*",
-                destination:
-                    process.env.NODE_ENV === "development"
-                        ? "http://127.0.0.1:8000/:path*"
-                        : "/api/",
-            },
-          /*  {
-                source: "/docs",
-                destination:
-                    process.env.NODE_ENV === "development"
-                        ? "http://127.0.0.1:8000/docs"
-                        : "/api/docs",
-            },
-            {
-                source: "/openapi.json",
-                destination:
-                    process.env.NODE_ENV === "development"
-                        ? "http://127.0.0.1:8000/openapi.json"
-                        : "/api/openapi.json",
-            },*/
-        ];
-    },
 };
 
 module.exports = nextConfig;
