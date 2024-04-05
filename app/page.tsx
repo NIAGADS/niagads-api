458e0099ace6952909571c4ba6d839834cec15b6
fossilfriend <egreenfest@gmail.com>
2024-04-05 09:56:34 -0400

    remove css / test live reload

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main >
-      <div>
-
+      <div className="pl-8 pt-8">
+            <h1 className="text-red-500">Hello World!</h1>
       </div>
     </main>
   );
