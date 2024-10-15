ac3d4bcb1dc4183969becdb30abadcf975e0c304
fossilfriend <egreenfest@gmail.com>
2024-10-15 13:19:31 -0400

    add options w/out ids

--- api/dependencies/shared_params.py
+++ api/dependencies/shared_params.py
@@ -6,8 +6,12 @@ from fastapi import Query
 
 # Query(Query()) nesting has to do w/FAST-API bug for documentation of Depends() params
 # see https://github.com/tiangolo/fastapi/issues/4700
+
 class OptionalParams(BaseModel):
-    limit: Optional[int] = Query(Query(default=50, description="maximum number of results to return; please note that `pagination is not yet implemented`"))
+    limit: Optional[int] = Query(Query(default=None, description="maximum number of results to return; please note that `pagination is not yet implemented`"))
     page: Optional[int] = Query(Query(default=None, description="current page; please note that `pagination is not yet implemented`"))
     countOnly: Optional[bool] = Query(Query(default = False, description="return result size (count) only"))
+
+class ExtendedOptionalParams(OptionalParams):
     idsOnly: Optional[bool] = Query(Query(default = False, description="return only the IDS (no annotation or metadata) for matching records"))
+    
