d872d18b93ef7b0d16900982efd20587423482a9
fossilfriend <egreenfest@gmail.com>
2024-10-23 16:13:55 -0400

    remove debugging statement

--- api/internal/config.py
+++ api/internal/config.py
@@ -1,10 +1,9 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_core import ValidationError
 from functools import lru_cache
 from os import path
 
 ENV_FILE_PATH = f"{path.dirname(path.abspath(__file__))}/../../.env.local"
-print(ENV_FILE_PATH)
-
 class Settings(BaseSettings):
     API_STATICDB_URL: str
     API_CACHEDB_URL: str
@@ -14,7 +13,8 @@ class Settings(BaseSettings):
     
     # required by Pydantic
     model_config = SettingsConfigDict(env_file=ENV_FILE_PATH)
-    
+  
+
 
 @lru_cache # memoization of functions
 def get_settings():
