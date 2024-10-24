f8ee5b6b652d1373393ba9130ebc3130b174fda3
fossilfriend <egreenfest@gmail.com>
2024-10-24 13:44:39 -0400

    untested enum for simplified mapping of data models

--- api/routers/view_redirects.py
+++ api/routers/view_redirects.py
@@ -1,24 +1,31 @@
 from fastapi import APIRouter, Request
-import json
+from enum import Enum
 
 from api.dependencies.exceptions import RESPONSES
 from api.response_models import GenomeBrowserConfig, RequestDataModel, SerializableModel, \
     VizTableResponse, VizTable, VizTableOptions
 
-from api.routers.filer.models import TrackPublic as FILERTrack, TrackOverlapSummary as FILERTrackOverlapSummary
+from api.routers.filer.models.track_response_model import FILERTrackBrief, FILERTrack, FILERTrackOverlapSummary
 
-def serialize(model, record: SerializableModel):
-    match model:
-        case 'filer_track':
-            return FILERTrack(**record)
-        case 'filer_overlaps_summary':
-            return FILERTrackOverlapSummary(**record)
-        case 'genome_browser_config':
-            return GenomeBrowserConfig(**record)
-        case _:
-            raise KeyError('Invalid data model for NIAGADS-viz-js Table: %s' % model)       
+class DataModel(Enum):
+    FILER_TRACK_BRIEF = FILERTrackBrief
+    FILER_TRACK = FILERTrack
+    FILER_TRACK_OVERLAP_SUMMARY = FILERTrackOverlapSummary
+    GENOME_BROWSER_CONFIG = GenomeBrowserConfig
+    
+
+def serialize(model: DataModel, record: SerializableModel):
+    try:
+        return model.value(**record)
+    except:
+        raise KeyError('Invalid data model for NIAGADS-viz-js Table: %s' % model)       
         
-def columns(model):
+def columns(model: DataModel):
+    try:
+        return model.value.view_table_columns()
+    except:
+        raise KeyError('Invalid data model for NIAGADS-viz-js Table: %s' % model)       
+    """
     match model:
         case 'filer_track':
             return FILERTrack.view_table_columns()
@@ -27,7 +34,8 @@ def columns(model):
         case 'genome_browser_config':
             return GenomeBrowserConfig.view_table_columns()
         case _:
-            raise KeyError('Invalid data model for NIAGADS-viz-js Table: %s' % model)       
+    """
+            
 
 
 TAGS = ['Views']
@@ -47,19 +55,24 @@ async def get_table_view(
     ) -> VizTableResponse:
 
     # get the original response from the request.session
-    requestData = request.session.get(forwardingRequestId + '_request')
-    requestData = request.session[forwardingRequestId + '_request']
-    responseData = request.session[forwardingRequestId + '_response'] # TODO: pulled from session
-    # try to convert the data
-    data = [ serialize(data_model, t) for t in responseData ] # convert Track to TrackPublic
-    
-    # build the table object
-    columns = columns(data_model)
-    columnIds = [c['id'] for c in columns]   
-    options = VizTableOptions(disableColumnFilters=True, defaultColumns=columnIds[:10])
-    table = VizTable(id='tracks', data=data, columns=columns, options=options)
-    
-    response = VizTableResponse(request=requestData, response=table)
-    #TODO - cache
-    return response
-   
+    try:
+        requestData = request.session.get(forwardingRequestId + '_request')
+        requestData = request.session[forwardingRequestId + '_request']
+        responseData = request.session[forwardingRequestId + '_response'] # TODO: pulled from session
+        # try to convert the data
+        data = [ serialize(data_model, t) for t in responseData ] # convert Track to TrackPublic
+        
+        # build the table object
+        columns = columns(data_model)
+        columnIds = [c['id'] for c in columns]   
+        options = VizTableOptions(disableColumnFilters=True, defaultColumns=columnIds[:10])
+        table = VizTable(id='tracks', data=data, columns=columns, options=options)
+        
+        response = VizTableResponse(request=requestData, response=table)
+        #TODO - cache
+        return response
+    except:
+        raise
+    finally:
+        del request.session[forwardingRequestId + '_response']
+        del request.session[forwardingRequestId + '_request']
