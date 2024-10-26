66b81f46a6ac357209e51f5340b902ecd8711a42
fossilfriend <egreenfest@gmail.com>
2024-10-25 21:56:27 -0400

    placdeholder for redirects

--- api/routers/filer/common/helpers.py
+++ api/routers/filer/common/helpers.py
@@ -9,14 +9,6 @@ from api.dependencies.param_validation import clean, convert_str2list
 from .services import MetadataQueryService, ApiWrapperService
 from ..dependencies import InternalRequestParameters
 
-def response_type(rType: ResponseType, default: Any):
-    match rType:
-        case ResponseType.TABLE:
-            return VizTableResponse
-        # case ResponseType.JSON:
-        #     return default
-        case _:
-            return default
 
 class HelperParameters(__BaseHelperParameters):
     internal: InternalRequestParameters
@@ -24,5 +16,19 @@ class HelperParameters(__BaseHelperParameters):
 
 async def get_track_metadata(opts: HelperParameters):
     result = await MetadataQueryService(opts.internal.session).get_track_metadata(convert_str2list(opts.parameters['track']))
-    model = response_type(ResponseType(opts.format), opts.responseModel)
-    return model(request=opts.internal.requestData, response=result)
+    match opts.format:
+        case ResponseType.TABLE:
+            raise NotImplementedError('Interactive Table View coming soon')
+            # ResponseRedirect()
+        case _:
+            return opts.model(request=opts.internal.requestData, response=result)
+        
+        
+    """ 
+    else:
+        # FIXME: cache in memory store; revisit when caching is set up
+        request.session[requestData.request_id + '_response'] = [t.serialize(promoteObjs=True, collapseUrls=True) for t in result]
+        request.session[requestData.request_id + '_request'] = requestData.serialize()
+        # redirectUrl = f'/view/table/filer_track?forwardingRequestId={requestData.request_id}'
+        # return RedirectResponse(url=redirectUrl, status_code=status.HTTP_303_SEE_OTHER)
+    """
