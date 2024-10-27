6113e1e3487804ed994734dfc06d94c029590b9f
fossilfriend <egreenfest@gmail.com>
2024-10-26 21:22:30 -0400

    abstract out validation, paged response

--- api/routers/filer/common/helpers.py
+++ api/routers/filer/common/helpers.py
@@ -3,11 +3,10 @@ from fastapi import status
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import RedirectResponse
 
-from api.common.helpers import HelperParameters as __BaseHelperParameters
+from api.common.helpers import HelperParameters as __BaseHelperParameters, Parameters
 from api.common.formatters import clean, convert_str2list
 from api.dependencies.parameters.optional import ResponseType
-from api.response_models.base_models import BaseResponseModel, RequestDataModel
-from api.response_models.data_models import GenericData
+from api.response_models.base_models import BaseResponseModel, RequestDataModel, PaginationDataModel
 
 from .services import MetadataQueryService, ApiWrapperService
 from ..dependencies import InternalRequestParameters
@@ -28,21 +27,35 @@ async def __generate_cache_key(requestData: RequestDataModel, model: BaseRespons
 # cache -> expected response as requestID_response, 
 # cache requestData as requestID_request so that original URL and params are passed   
 
+def __validate_params(format: ResponseType, opts: Parameters ):
+    countsOnly = getattr(opts, 'countsOnly', False)
+    idsOnly = getattr(opts, 'idsOnly', False )
+    if countsOnly and opts.format != ResponseType.JSON:
+        raise RequestValidationError(f'Invalid response format selected: `{opts.format.value}`; counts can only be returned in a `JSON` response')
+    if idsOnly and countsOnly:
+        raise RequestValidationError("please set only one of `idsOnly` or `countsOnly` to `true`")
+
 def __generate_response(result: Any, opts:HelperParameters):
     rowModel = opts.model.row_model(name=True)
     requestId = opts.internal.requestData.request_id
+    isPaged = opts.model.is_paged()
+    if isPaged:
+        numRecords = len(result)
+        pagination = PaginationDataModel(page=1, total_num_pages=1, paged_num_records=numRecords, total_num_records=numRecords)
+
     match opts.format:
         case ResponseType.TABLE:
             redirectUrl = f'/view/table/{rowModel}?forwardingRequestId={requestId}'
             return RedirectResponse(url=redirectUrl, status_code=status.HTTP_303_SEE_OTHER)
         case _:
+            if isPaged:
+                return opts.model(request=opts.internal.requestData, pagination=pagination, response=result)
             return opts.model(request=opts.internal.requestData, response=result)
 
 
 async def get_track_data(opts: HelperParameters):
-    countsOnly = getattr(opts.parameters, 'countsOnly', False)
-    if countsOnly and opts.format != ResponseType.JSON:
-        raise RequestValidationError(f'Invalid response format selected: `{opts.format.value}`; counts can only be returned in a `JSON` response')
+    __validate_params(opts.format, opts.parameters)
+    countsOnly = getattr(opts, 'countsOnly', False)
     
     await MetadataQueryService(opts.internal.session).validate_tracks(convert_str2list(opts.parameters.track))
     result = await ApiWrapperService().get_track_hits(clean(opts.parameters.track), opts.parameters.span, countsOnly=countsOnly)
@@ -59,4 +72,5 @@ async def search_track_metadata(opts: HelperParameters):
     result =  await MetadataQueryService(opts.internal.session) \
         .query_track_metadata(opts.parameters.assembly, 
             opts.parameters.filter, opts.parameters.keyword, opts.parameters.options)
+        
     return __generate_response(result, opts)
