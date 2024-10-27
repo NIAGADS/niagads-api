da2d44a3909d845d4bc1d4a1dc7219dd2a54e078
fossilfriend <egreenfest@gmail.com>
2024-10-26 21:22:55 -0400

    search

--- api/routers/filer/routes/metadata.py
+++ api/routers/filer/routes/metadata.py
@@ -8,6 +8,7 @@ from api.dependencies.parameters.location import Assembly, assembly_param
 from api.dependencies.parameters.optional import PaginationParameters, counts_only_param, format_param, ids_only_param, keyword_param
 from api.response_models import GenomeBrowserConfigResponse, GenomeBrowserExtendedConfigResponse
 from api.common.helpers import Parameters
+from api.response_models.base_models import BaseResponseModel
 
 from ..common.helpers import HelperParameters, get_track_metadata as __get_track_metadata, search_track_metadata as __search_track_metadata
 from ..common.constants import ROUTE_TAGS, TRACK_SEARCH_FILTER_FIELD_MAP
@@ -35,7 +36,7 @@ async def get_track_metadata(
 
 tags = TAGS + ['Record(s) by Text Search'] + ['Track Metadata by Text Search']
 filter_param = FilterParameter(TRACK_SEARCH_FILTER_FIELD_MAP, ExpressionType.TEXT)
-@router.get("/search", tags=tags, response_model=FILERTrackPagedResponse,
+@router.get("/search", tags=tags, response_model=Union[BaseResponseModel, FILERTrackPagedResponse],
     name="Search for tracks", 
     description="find functional genomics tracks using category filters or by a keyword search against all text fields in the track metadata")
 async def search_track_metadata(
@@ -47,16 +48,19 @@ async def search_track_metadata(
         countsOnly = Depends(counts_only_param),
         idsOnly = Depends(ids_only_param),
         internal: InternalRequestParameters = Depends(),
-        ) -> FILERTrackPagedResponse:
+        ) -> Union[BaseResponseModel, FILERTrackPagedResponse]:
     
     if filter is None and keyword is None:
         raise RequestValidationError('must specify either a `filter` and/or a `keyword` to search')
     
     optionalParameters = Parameters(countsOnly=countsOnly, idsOnly=idsOnly)
     
+    responseModel = BaseResponseModel if countsOnly or idsOnly else FILERTrackPagedResponse
+    
     opts = HelperParameters(internal=internal, pagination=pagination,
-        format=format, model=FILERTrackPagedResponse,
+        format=format, model=responseModel,
         parameters=Parameters(assembly=assembly, filter=filter, keyword=keyword, options=optionalParameters))
+    
     return await __search_track_metadata(opts)
 
 
