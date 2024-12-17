218a08095c64c65aa45fe260050851e887f1ab8e
fossilfriend <egreenfest@gmail.com>
2024-12-17 11:22:56 -0500

    progress on templating out helpers and pagination

--- api/common/helpers.py
+++ api/common/helpers.py
@@ -4,27 +4,24 @@ from fastapi import status
 from pydantic import BaseModel, field_validator, ConfigDict
 from typing import Any, Dict
 
+from api.common.constants import DEFAULT_PAGE_SIZE, MAX_NUM_PAGES
 from api.common.enums import ResponseContent, CacheNamespace
+from api.common.utils import get_attribute
 from api.dependencies.parameters.services import InternalRequestParameters
 from api.dependencies.parameters.optional import ResponseFormat
 from api.models.base_models import BaseResponseModel, PaginationDataModel
 from api.routers.redirect.common.constants import RedirectEndpoints
 
-# basically allow creation of an arbitrary namespace
 class Parameters(BaseModel):
+    """ arbitrary namespace to store request parameters and pass them to helpers """
     __pydantic_extra__: Dict[str, Any]  
     model_config = ConfigDict(extra='allow')
     
-class HelperParameters(BaseModel, arbitrary_types_allowed=True):
-    internal: InternalRequestParameters
-    model: Any
+    
+class ResponseConfiguration(BaseModel, arbitrary_types_allowed=True):
     format: ResponseFormat = ResponseFormat.JSON
     content: ResponseContent = ResponseContent.FULL
-    pagination: PaginationDataModel = None
-    parameters: Parameters = None
-    
-    # __pydantic_extra__: Dict[str, Any]  
-    # model_config = ConfigDict(extra='allow')
+    model: Any = None
     
     # from https://stackoverflow.com/a/67366461
     # allows ensurance taht model is always a child of BaseResponseModel
@@ -41,46 +38,112 @@ class HelperParameters(BaseModel, arbitrary_types_allowed=True):
         except NameError:
             raise RequestValidationError(f'Invalid value provided for `content`: {content}')
 
-
-def __set_pagination(opts: HelperParameters, resultSize):
-    if opts.model.is_paged():
-        page = 1 if opts.pagination is None else opts.pagination.page
-        nPages = getattr(opts.pagination, 'total_num_pages', 1)
-        expectedResultSize = getattr(opts.pagination, 'total_num_records', resultSize)
-        return PaginationDataModel(
-            page=page, 
-            total_num_pages= nPages, 
-            paged_num_records=resultSize, 
-            total_num_records=expectedResultSize
-        )
-    return None
-
-
-async def generate_response(result: Any, opts:HelperParameters, isCached=False):
-    if not isCached:
-        response = None
-        opts.internal.requestData.update_parameters(opts.parameters, exclude=['span', '_track'])
-        if opts.model.is_paged():
-            pagination: PaginationDataModel = __set_pagination(opts, len(result))
-            response =  opts.model(request=opts.internal.requestData, pagination=pagination, response=result)
-        else: 
-            response = opts.model(request=opts.internal.requestData, response=result)
-        # cache the response
-        await opts.internal.internalCache.set(opts.internal.cacheKey.internal, response, namespace=opts.internal.cacheKey.namespace)
-    else:
-        response = result
+class RouteHelper():
+    
+    def __init__(self, managers: InternalRequestParameters, responseConfig: ResponseConfiguration, params: Parameters):
+        self._managers: InternalRequestParameters = managers
+        self._responseConfig: ResponseConfiguration = responseConfig
+        self._pagination: PaginationDataModel = None
+        self._parameters: Parameters = params
+        self._pageSize: int = MAX_NUM_PAGES
+        self._resultSize: int = None
+    
+    
+    def set_page_size(self, pageSize: int):
+        self._pageSize = pageSize
         
-    match opts.format:
-        case ResponseFormat.TABLE:
-            # cache the response again, this time by the requestId b/c 
-            # the cacheKey cannot be passed through the URL  
-            cacheKey = opts.internal.cacheKey.external   
-            requestIsCached = await opts.internal.internalCache.exists(cacheKey, namespace=CacheNamespace.VIEW)
-            if not requestIsCached:  # then cache it
-                await opts.internal.internalCache.set(cacheKey, response, namespace=CacheNamespace.VIEW)
+    
+    def _pagination_exists(self):
+        if self._pagination is None:
+            raise RuntimeError('Attempting to modify or access pagination before initializing')
+
+
+    def _is_valid_page(self, page: int):
+        """ test if the page is valid (w/in range of expected number of pages)"""     
+
+        self._pagination_exists()
+    
+        if self._pagination.total_num_pages is None:
+            raise RuntimeError('Attempting fetch a page before estimating total number of pages')
+        
+        if page > self._pagination.total_num_pages:
+            raise RequestValidationError(f'Request `page` {page} does not exist; this query generates a maximum of {self._pagination.total_num_pages} pages')
+        
+        return True
+    
+    
+    def page(self):
+        if self._parameters is not None:
+            return get_attribute(self._parameters, 'page', 1)
+        return 1
+    
+    
+    def total_num_pages(self):
+        if self._resultSize is None:
+            raise RuntimeError('Attempting to page before estimating result size.')
+        
+        return 1 if self._resultSize < self._pageSize \
+            else next((p for p in range(1, MAX_NUM_PAGES) if (p - 1) * self._pageSize > self._resultSize)) - 1
             
-            redirectUrl = f'/redirect{RedirectEndpoints.TABLE.value}/{cacheKey}'
-            return RedirectResponse(url=redirectUrl, status_code=status.HTTP_303_SEE_OTHER)
-        case _:  
-            return response
+
+    def initialize_pagination(self,):
+        if self._responseConfig.model.is_paged():
+            self._pagination = PaginationDataModel(
+                page=self.page(),
+                total_num_pages=self.total_num_pages(),
+                paged_num_records=None,
+                total_num_records=self._resultSize)
+            
+            self._is_valid_page(self._pagination.page)
+        else:
+            raise RuntimeError('Attempt to page a non-pageable response model')
+
+
+    def set_paged_num_records(self, numRecords: int):
+        self._pagination_exists()
+        self._pagination.paged_num_records = numRecords
+
+
+    def offset(self):
+        """ calculate offset for SQL pagination""" 
+        self._pagination_exists()
+        return None if self._pagination.page == 1 \
+            else (self._pagination.page - 1) * self._pageSize
+
+    
+    async def generate_response(self, result: Any, isCached=False):
+        response = result if isCached else None
+        if response is None:
+            self._managers.requestData.update_parameters(self._parameters, exclude=['span', '_track'])
+
+            if self._pagination_exists():
+                self.set_paged_num_records(len(result))
+                response = self._responseConfig.model(
+                    request=self._managers.requestData,
+                    pagination=self._pagination,
+                    response=result)
+            else: 
+                response = self._responseConfig.model(
+                    request=self._managers.requestData, 
+                    response=result)
+                
+            # cache the response
+            await self._managers.internalCache.set(
+                self._managers.cacheKey.internal, 
+                response, 
+                namespace=self._managers.cacheKey.namespace)
+            
+        match self._responseConfig.format:
+            case ResponseFormat.TABLE:
+                # cache the response again, this time by the requestId b/c 
+                # the cacheKey cannot be passed through the URL  
+                cacheKey = self._managers.cacheKey.external   
+                requestIsCached = await self._managers.internalCache.exists(cacheKey, namespace=CacheNamespace.VIEW)
+                if not requestIsCached:  # then cache it
+                    await self._managers.internalCache.set(cacheKey, response, namespace=CacheNamespace.VIEW)
+                
+                redirectUrl = f'/redirect{RedirectEndpoints.TABLE.value}/{cacheKey}'
+                return RedirectResponse(url=redirectUrl, status_code=status.HTTP_303_SEE_OTHER)
+            case _:  
+                return response
 
--- api/routers/filer/common/helpers.py
+++ api/routers/filer/common/helpers.py
@@ -1,5 +1,5 @@
 import asyncio
-from typing import List
+from typing import Any, List
 from fastapi.exceptions import RequestValidationError
 from sqlalchemy.ext.asyncio import AsyncSession
 from collections import ChainMap
@@ -9,10 +9,10 @@ from operator import itemgetter
 from niagads.utils.list import cumulative_sum, chunker
 
 from api.common.enums import ResponseContent, CacheNamespace
-from api.common.helpers import HelperParameters as __BaseHelperParameters, generate_response as __generate_response
+from api.common.helpers import Parameters, ResponseConfiguration, RouteHelper
 from api.common.utils import get_attribute
 
-from .constants import TRACKS_PER_API_REQUEST_LIMIT, DEFAULT_PAGE_SIZE, MAX_NUM_PAGES
+from .constants import TRACKS_PER_API_REQUEST_LIMIT
 from .enums import FILERApiEndpoint
 from .services import MetadataQueryService, ApiWrapperService
 from ..dependencies import InternalRequestParameters
@@ -21,162 +21,133 @@ from ..models.track_metadata_cache import Track
 import logging
 LOGGER = logging.getLogger(__name__)
 
-
-class HelperParameters(__BaseHelperParameters):
-    internal: InternalRequestParameters
     
-class FILERRouteHelper():  
-    __options: HelperParameters
-    __pageSize: int = DEFAULT_PAGE_SIZE
-    __offset: int = None
-    __totalNumPages: int = None
-    __resultSize: int = None
-
-    def init(self, opts: HelperParameters):
-        self.__options = opts
-        
-        
-    def set_page_size(self, pageSize: int):
-        self.__pageSize = pageSize
-        
-        
-    def __set_total_num_pages(self):
-        self.__totalNumPages = 1 if self.__resultSize < self.__pageSize \
-            else next((p for p in range(1, MAX_NUM_PAGES) if (p - 1) * self.__pageSize > self.__resultSize)) - 1
-        
-    def __is_valid_page(self, page: int):
-        """ test if the page is valid (w/in range of expected number of pages)"""     
-        if page > self.__totalNumPages:
-            raise RequestValidationError(f'Request `page` {page} does not exist; this query generates a maximum of {self.__totalNumPages} pages')
-        
-        return True
+class FILERRouteHelper(RouteHelper):  
     
-    def __page_metadata_query(self, page: int):
-        """ calculate offset and total number of pages for pagination"""
-        self.__set_total_num_pages()
-        self.__is_valid_page()
+    def __init__(self, managers: InternalRequestParameters, responseConfig: ResponseConfiguration, params: Parameters):
+        super().__init__(managers, responseConfig, params)
+        self._managers: InternalRequestParameters = managers
+    
 
-        self.__offset = None if page == 1 else (page - 1) * self.__pageSize
-
-        
-    def __page_track_data_query(self, trackSummary: dict, page: int):
-        """ calculate expected result size, number of pages; returns current set of paged tracks"""
-        # returns pagedTrackIds, resultSize, numPages
+    def __page_data_query(self, trackSummary: dict, page: int):
+        """ calculate expected result size, number of pages; 
+            returns current set of paged tracks"""
         
         # calculate cumulative sum of expected hits per track 
         sortedTrackSummary = sorted(trackSummary, key = lambda item: item['num_overlaps'], reverse=True)
         cumulativeSum = cumulative_sum([t['num_overlaps'] for t in sortedTrackSummary])
-        self.__resultSize = cumulativeSum[-1] # last element is total number of hits
+        self._resultSize = cumulativeSum[-1] # last element is total number of hits
         
-        self.__set_total_num_pages()
-        self.__is_valid_page()
+        self.initialize_pagination()
         
         pagedTracks = None
-        if self.__resultSize <= self.__pageSize and page == 1:
-            return [t['track_id'] for t in trackSummary]
+        if self._resultSize <= self._pageSize:
+            pagedTracks = [t['track_id'] for t in trackSummary]
         
-        minRecordRowIndex = (page - 1) * self.__pageSize
-        maxRecordRowIndex = minRecordRowIndex + self.__pageSize
-        if maxRecordRowIndex > expectedResultSize:
-            maxRecordRowIndex = expectedResultSize
+        else:
+            minRecordRowIndex = (page - 1) * self._pageSize
+            maxRecordRowIndex = minRecordRowIndex + self._pageSize
+            if maxRecordRowIndex > self._resultSize:
+                maxRecordRowIndex = self._resultSize
+            
+            # NOTE: () in the `next` returns an iterator instead of a list 
+            # (i.e. instead of []) for dictionary comprehension
+            pageStartIndex = next((index for index, counts in enumerate(cumulativeSum) if counts >= minRecordRowIndex))
+            pageEndIndex = len(cumulativeSum) - 1 if maxRecordRowIndex == self._resultSize \
+                else next((index for index, counts in enumerate(cumulativeSum) if counts >= maxRecordRowIndex)) - 1
+            pagedTracks = [t['track_id'] for t in sortedTrackSummary[pageStartIndex:pageEndIndex]]
         
-        
-        # () returns an iterator instead of a list; i.e. []
-        pageStartIndex = next((index for index, counts in enumerate(cumulativeSum) if counts >= minRecordRowIndex))
-        pageEndIndex = len(cumulativeSum) - 1 if maxRecordRowIndex == expectedResultSize \
-            else next((index for index, counts in enumerate(cumulativeSum) if counts >= maxRecordRowIndex)) - 1
-        pagedTracks = [t['track_id'] for t in sortedTrackSummary[pageStartIndex:pageEndIndex]]
-        
-        return pagedTracks, expectedResultSize, expectedNumPages
-        
+        return pagedTracks
 
     
     def __merge_track_lists(self, trackList1, trackList2):
         matched = groupby(sorted(trackList1 + trackList2, key=itemgetter('track_id')), itemgetter('track_id'))
         combinedLists = [dict(ChainMap(*g)) for k, g in matched]
         return combinedLists
+    
 
-    async def __validate_tracks(session: AsyncSession, tracks: List[str]):
+    async def __validate_tracks(self, tracks: List[str]):
         """ by setting validate=True, the service runs .validate_tracks before validating the genome build"""
-        assembly = await MetadataQueryService(session).get_genome_build(tracks, validate=True)
+        assembly = await MetadataQueryService(self._managers.session).get_genome_build(tracks, validate=True)
         if isinstance(assembly, dict):
             raise RequestValidationError(f'Tracks map to multiple assemblies; please query GRCh37 and GRCh38 data independently')
         return assembly
 
 
-    async def __get_track_data_task(tracks, assembly: str, span: str, countsOnly: bool, cacheKey: str, managers:InternalRequestParameters):
+    async def __get_track_data_task(self, tracks, assembly: str, span: str, countsOnly: bool, cacheKey: str):
         cacheKey += ','.join(tracks)
-        result = await managers.internalCache.get(cacheKey, namespace=CacheNamespace.FILER_EXTERNAL_API)
+        result = await self._managers.internalCache.get(cacheKey, namespace=CacheNamespace.FILER_EXTERNAL_API)
         if result is None:   
-            result = await ApiWrapperService(managers.apiClientSession).get_track_hits(tracks, span, assembly, countsOnly=countsOnly)
-            await managers.internalCache.set(cacheKey, result, namespace=CacheNamespace.FILER_EXTERNAL_API)
+            result = await ApiWrapperService(self._managers.apiClientSession).get_track_hits(tracks, span, assembly, countsOnly=countsOnly)
+            await self._managers.internalCache.set(cacheKey, result, namespace=CacheNamespace.FILER_EXTERNAL_API)
         return result
 
 
-    async def get_track_data(opts: HelperParameters, validate=True): 
+    async def get_track_data(self, validate=True): 
         isCached = True # assuming true from the start
-        result = await opts.internal.internalCache.get(opts.internal.cacheKey.internal, namespace=opts.internal.cacheKey.namespace)
+        cacheKey = self._managers.cacheKey
+        
+        result = await self._managers.internalCache.get(cacheKey.internal, namespace=cacheKey.namespace)
 
         if result is None:
             isCached = False
             
-            summarize = opts.content == ResponseContent.SUMMARY
-            countsOnly = opts.content == ResponseContent.COUNTS or summarize == True # if summarize is true, we only want counts
+            summarize = self._responseConfig.content == ResponseContent.SUMMARY
+            countsOnly = self._responseConfig.content == ResponseContent.COUNTS or summarize == True 
             
-            # FIXME: getattr raises KeyError / ternary not working either -> related to Pydantic
-            # tracks = None
-            # if 'track' in opts.parameters:
-            #     tracks = opts.parameters.track
-            # else:
-            #     tracks = opts.parameters._track
-            
-            tracks = get_attribute(opts.parameters, 'track', get_attribute(opts.parameters, '_track'))
+            tracks = get_attribute(self._parameters, 'track', get_attribute(self._parameters, '_track'))
             tracks = tracks.split(',') if isinstance(tracks, str) else tracks
             tracks = sorted(tracks) # best for caching
             
-            assembly = get_attribute(opts.parameters, 'assembly')
+            assembly = get_attribute(self._parameters, 'assembly')
             if validate or assembly is None: # for internal helper calls, don't always need to validate; already done
-                assembly = await __validate_tracks(opts.internal.session, tracks)         
+                assembly = await self.__validate_tracks(tracks)         
                 
-            cacheKey = f'/{FILERApiEndpoint.OVERLAPS}?countsOnly={countsOnly}&span={opts.parameters.span}&tracks=' 
+            cacheKey = f'/{FILERApiEndpoint.OVERLAPS}?countsOnly={countsOnly}&span={self._parameters.span}&tracks=' 
             cacheKey = cacheKey.replace(':', '_')
             
             if countsOnly: # summarize & counts only do not need to be chunked
-                result = await __get_track_data_task(tracks, assembly, opts.parameters.span, countsOnly, cacheKey, opts.internal)
+                result = await self.__get_track_data_task(tracks, assembly, self._parameters.span, countsOnly, cacheKey)
             
                 if summarize: # summarize doesn't need to be chunked b/c DB query
-                    metadata: List[Track] = await get_track_metadata(opts, rawResponse=True)
-                    result = __merge_track_lists([t.serialize(promoteObjs=True) for t in metadata], [t.serialize() for t in result])
+                    metadata: List[Track] = await self.get_track_metadata(rawResponse=True)
+                    result = self.__merge_track_lists(
+                        [t.serialize(promoteObjs=True) for t in metadata], 
+                        [t.serialize() for t in result])
                     result = sorted(result, key = lambda item: item['num_overlaps'], reverse=True)
                     # this step will be cached in __generate_response
                     
             else: # make parallel calls for large lists of tracks
                 chunks = chunker(tracks, TRACKS_PER_API_REQUEST_LIMIT, returnIterator=True)
-                tasks = [__get_track_data_task(c, assembly, opts.parameters.span, countsOnly, cacheKey, opts.internal) for c in chunks]
+                tasks = [self.__get_track_data_task(c, assembly, self._parameters.span, countsOnly, cacheKey) for c in chunks]
                 result = await asyncio.gather(tasks)
                 
-        return await __generate_response(result, opts, isCached=isCached)
+        return await self.generate_response(result, isCached=isCached)
 
 
-    async def get_track_metadata(opts: HelperParameters, rawResponse=False):
+    async def get_track_metadata(self, rawResponse=False):
+        """ fetch track metadata; expects a list of track identifiers in the parameters"""
         isCached = True # assuming true from the start
-        result = await opts.internal.internalCache.get(opts.internal.cacheKey.internal, namespace=opts.internal.cacheKey.namespace)
+        
+        result = await self._managers.internalCache.get(
+            self._managers.cacheKey.internal, 
+            namespace=self._managers.cacheKey.namespace)
+        
         if result is None:
             isCached = False
-            
-            # FIXME: get/getattr not working w/default -- Pydantic issue / also ternary not working
-            tracks = None
-            if 'track' in opts.parameters:
-                tracks = opts.parameters.track
-            else:
-                tracks = opts.parameters._track
+        
+            tracks = get_attribute(self._parameters, 'track', get_attribute(self._parameters, '_track'))
             tracks = tracks.split(',') if isinstance(tracks, str) else tracks
-            
-            result = await MetadataQueryService(opts.internal.session).get_track_metadata(tracks)
+            tracks = sorted(tracks) # best for caching
+
+            result = await MetadataQueryService(self._managers.session).get_track_metadata(tracks)
             
         if rawResponse:
             return result
-        return await __generate_response(result, opts, isCached=isCached)
+        
+        else:
+            pass # TODO: paginate
+        return await self.generate_response(result, isCached=isCached)
 
 
     async def search_track_metadata(self, opts: HelperParameters):
@@ -189,9 +160,9 @@ class FILERRouteHelper():
                 counts = await MetadataQueryService(opts.internal.session) \
                     .query_track_metadata(opts.parameters.assembly, 
                         opts.parameters.filter, opts.parameters.keyword, ResponseContent.COUNTS)
-                self.__resultSize = counts['track_count']
+                self._resultSize = counts['track_count']
                 self.__page_metadata_query(opts.pagination.page)
-                opts.pagination.total_num_records = self.__resultSize
+                opts.pagination.total_num_records = self._resultSize
                 opts.pagination.total_num_pages = self.__totalNumPages
 
             result = await MetadataQueryService(opts.internal.session) \
@@ -201,7 +172,7 @@ class FILERRouteHelper():
         return await __generate_response(result, opts, isCached=isCached)
 
 
-    async def search_track_data(opts: HelperParameters):
+    async def search_track_data(self):
         result = await opts.internal.internalCache.get(opts.internal.cacheKey.internal, namespace=opts.internal.cacheKey.namespace)
         if result is not None: # just return the response
             return await __generate_response(result, opts, isCached=True)
@@ -233,7 +204,7 @@ class FILERRouteHelper():
         LOGGER.info("target tracks found")
         
         if opts.content == ResponseContent.FULL:
-            pagedTrackIds, resultSize, numPages = __page_track_data_query(targetTracks, page=opts.pagination.page)
+            pagedTrackIds = self.__page_data_query(targetTracks, page=opts.pagination.page)
             opts.parameters._track = pagedTrackIds
             opts.pagination.total_num_records = resultSize
             opts.pagination.total_num_pages = numPages
