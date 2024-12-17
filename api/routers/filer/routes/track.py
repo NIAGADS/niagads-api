427d0027f8dd520afeb89a7400fcdd5417e5ab68
fossilfriend <egreenfest@gmail.com>
2024-12-17 14:55:11 -0500

    adapt to user Helper class

--- api/routers/filer/routes/track.py
+++ api/routers/filer/routes/track.py
@@ -6,15 +6,14 @@ from api.common.exceptions import RESPONSES
 from api.common.formatters import print_enum_values
 from api.dependencies.parameters.location import span_param
 from api.dependencies.parameters.optional import format_param, get_response_content, validate_response_content
-from api.common.helpers import Parameters
+from api.common.helpers import Parameters, ResponseConfiguration
 
 from api.models import GenomeBrowserConfigResponse, GenomeBrowserExtendedConfigResponse, BEDResponse
 from api.models.base_models import BaseResponseModel
 
 from ..common.constants import ROUTE_TAGS
 from ..dependencies import InternalRequestParameters, path_track_id
-from ..common.helpers import (get_track_metadata as __get_track_metadata, 
-    get_track_data as __get_track_data, HelperParameters)
+from ..common.helpers import FILERRouteHelper
 from ..models.response.filer_track import FILERTrackResponse, FILERTrackBriefResponse
 
 # TAGS = ROUTE_TAGS
@@ -37,12 +36,17 @@ async def get_track_metadata(
         internal: InternalRequestParameters = Depends()
         ) -> Union[FILERTrackBriefResponse, FILERTrackResponse]:
     
-    content = await validate_response_content(get_track_metadata_content_enum, content)
-    responseModel = FILERTrackResponse if content == ResponseContent.FULL else FILERTrackBriefResponse
-    opts = HelperParameters(internal=internal, 
-        model=responseModel, content=content,
-        parameters=Parameters(track=track))
-    return await __get_track_metadata(opts)
+    rContent = validate_response_content(get_track_metadata_content_enum, content)
+    helper = FILERRouteHelper(
+        internal,
+        ResponseConfiguration(
+            content=rContent,
+            model = FILERTrackResponse if rContent == ResponseContent.FULL \
+                else FILERTrackBriefResponse
+        ),
+        Parameters(track=track))
+    
+    return await helper.get_track_metadata()
 
 
 tags = ["Record by ID", "NIAGADS Genome Browser Configuration"]
@@ -52,18 +56,26 @@ tags = ["Record by ID", "NIAGADS Genome Browser Configuration"]
     description="retrieve NIAGADS Genome Browser track configuration for the FILER `track` specified in the path")
 async def get_track_browser_config(
         track = Depends(path_track_id),
-        inclMetadata: Optional[bool] = Query(default=False, description="include filterable track metadata for the track selector display"),
+        content: str = Query(ResponseContent.SUMMARY, description=f'response content; one of: {print_enum_values(get_track_metadata_content_enum)}'),
         internal: InternalRequestParameters = Depends()) \
     -> Union[GenomeBrowserConfigResponse, GenomeBrowserExtendedConfigResponse]:
         
-    responseModel = GenomeBrowserExtendedConfigResponse if inclMetadata \
-        else GenomeBrowserConfigResponse
-    opts = HelperParameters(internal=internal, model=responseModel, parameters=Parameters(track=track))
-    return await __get_track_metadata(opts)
+    rContent = validate_response_content(get_track_metadata_content_enum, content)
+    helper = FILERRouteHelper(
+        internal,
+        ResponseConfiguration(
+            content=rContent,
+            model=GenomeBrowserExtendedConfigResponse \
+                if rContent == ResponseContent.FULL \
+                else GenomeBrowserConfigResponse
+        ),
+        Parameters(track=track)
+    )
+    
+    return await helper.get_track_metadata()
 
 
 tags = ["Record by ID", "Track Data by ID"]
-
 get_track_data_content_enum = get_response_content(exclude=[ResponseContent.IDS, ResponseContent.SUMMARY])
 @router.get("/{track}/data", tags=tags, 
     name="Get track data", response_model=Union[BEDResponse, BaseResponseModel],
@@ -76,11 +88,18 @@ async def get_track_data(
         internal: InternalRequestParameters = Depends()
         ) -> Union[BEDResponse, BaseResponseModel]:
     
-    content = await validate_response_content(get_track_data_content_enum, content)
-    responseModel = BEDResponse if content == ResponseContent.FULL else BaseResponseModel
-    opts = HelperParameters(internal=internal, format=format, 
-        model=responseModel, content=content,
-        parameters=Parameters(track=track, span=span))
-    return await __get_track_data(opts)
+    rContent = await validate_response_content(get_track_data_content_enum, content)
+    helper = FILERRouteHelper(
+        internal,
+        ResponseConfiguration(
+            content=rContent,
+            format=format,
+            model=BEDResponse if content == ResponseContent.FULL \
+                else BaseResponseModel
+        ),
+        Parameters(track=track, span=span)
+    )
+
+    return await helper.get_track_data()
 
 
