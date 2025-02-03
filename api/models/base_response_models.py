c0f3addff278cc36bb769d9762c8ad859abd9c91
fossilfriend <egreenfest@gmail.com>
2025-02-03 15:59:10 -0500

    move abstractresponse to base_response_models

--- api/models/base_response_models.py
+++ api/models/base_response_models.py
@@ -1,17 +1,64 @@
+from abc import ABC, abstractmethod
 from pydantic import Field
-from typing import TypeVar
+from typing import Any, Dict, TypeVar
 from typing_extensions import Self
 
-from api.models.base_models import AbstractResponse, PaginationDataModel, RequestDataModel
+from api.common.enums import OnRowSelect, ResponseFormat, ResponseView
+from api.models.base_models import PaginationDataModel, RequestDataModel, RowModel, SerializableModel
+
+
+# FIXME: 'Any' or 'SerializableModel' for response
+class AbstractResponse(ABC, SerializableModel):
+    response: Any = Field(description="result (data) from the request")
+    
+    @abstractmethod
+    def to_text(self, format:ResponseFormat, **kwargs):
+        """ return a text response (e.g., BED, VCF, Download script) """
+        raise RuntimeError('`AbstractModel` is an abstract class; need to override abstract methods in child classes')
+    
+    @abstractmethod 
+    def to_view(self, view:ResponseView, **kwargs):
+        """ transform response to JSON expected by NIAGADS-viz-js Table """
+        raise RuntimeError('`AbstractModel` is an abstract class; need to override abstract methods in child classes')
 
 class BaseResponseModel(AbstractResponse):
     request: RequestDataModel = Field(description="details about the originating request that generated the response")
 
-    def to_view(self, view, **kwargs):
-        return super().to_view(view, **kwargs)
+    def to_view(self, view: ResponseView, **kwargs):
+        """ transform response to JSON expected by NIAGADS-viz-js Table """
+        if len(self.response) == 0:
+            raise RuntimeError('zero-length response; cannot generate view')
+        if 'on_row_select' not in kwargs:
+            if 'num_overlaps' in self.response[0].model_fields.keys() or \
+                'num_overlaps' in self.response[0].model_extra.keys():
+                kwargs['on_row_select'] = OnRowSelect.ACCESS_ROW_DATA
+                
+        viewResponse: Dict[str, Any] = {}
+        data = []
+        row: RowModel # annotated type hint
+        for index, row in enumerate(self.response):
+            if index == 0:
+                viewResponse = row.get_view_config(view, **kwargs)
+            data.append(row.to_view_data(view, **kwargs))
+        viewResponse.update({'data': data})
     
-    def to_text(self, format, **kwargs):
-        return super().to_text(format, **kwargs)
+        if view == ResponseView.TABLE:
+            viewResponse.update({'id': kwargs['id']})
+            
+        return viewResponse
+    
+    def to_text(self, format: ResponseFormat, **kwargs):
+        """ return a text response (e.g., BED, VCF, Download script) """
+        
+        responseStr = "" 
+        rowText = [] 
+        if len(self.response) > 0:
+            row: RowModel
+            for row in self.response:
+                rowText.append(row.to_text(format, **kwargs))        
+            responseStr = '\n'.join(rowText)
+        
+        return responseStr
         
     @classmethod
     def row_model(cls: Self, name=False):
@@ -31,9 +78,6 @@ class BaseResponseModel(AbstractResponse):
 
 class PagedResponseModel(BaseResponseModel):
     pagination: PaginationDataModel = Field(description="pagination details, if the result is paged")
-
-    # def to_view(self, view, **kwargs):
-    #     return super().to_view(view, **kwargs)
     
     
 # possibly allows you to set a type hint to a class and all its subclasses
