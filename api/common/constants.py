353b21ab51f500fac9eea593fdcdec871dba1d0a
fossilfriend <egreenfest@gmail.com>
2025-02-03 11:21:00 -0500

    clean up

--- api/common/constants.py
+++ api/common/constants.py
@@ -2,10 +2,6 @@ from typing import Any, Dict, List, Union
 
 JSON_TYPE = Union[Dict[str, Any], List[Any], int, float, str, bool, None]
 
-CHR_NS = [str(x) for x in [*range(1,22)]] + ['X', 'Y', 'N']
-CHR_IDS = [ 'chr' + str(x) for x in CHR_NS]
-CHROMOSOMES = [*CHR_NS, *CHR_IDS]
-
 ALLOWABLE_FILER_TRACK_FILTERS = {
     "dataSource": "original data source ", 
     "assay": "assay type", 
@@ -15,14 +11,11 @@ ALLOWABLE_FILER_TRACK_FILTERS = {
     "tissue": "tissue associated with biosample"
 }
 
-ADSP_VARIANTS_ACCESSION = "NG00067"
-
 FILER_N_TRACK_LOOKUP_LIMIT = 50
 
-
 # pagination
-DEFAULT_PAGE_SIZE = 3000
-MAX_NUM_PAGES = 500
+DEFAULT_PAGE_SIZE = 5000
+MAX_NUM_PAGES = 10
 
 # http client 
 HTTP_CLIENT_TIMEOUT=30 # timeout in seconds
