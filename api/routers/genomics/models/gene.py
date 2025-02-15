28a5f571ae8656094f0915027314221e27c35a3e
fossilfriend <egreenfest@gmail.com>
2025-02-15 11:41:58 -0500

    annotated gene

--- api/routers/genomics/models/gene.py
+++ api/routers/genomics/models/gene.py
@@ -1,5 +1,14 @@
-    
+from typing import Dict, List, Optional, Union
+
+from api.models.base_response_models import PagedResponseModel
 from api.models.genome import Gene
 
 class AnnotatedGene(Gene):
-    pass
+    type: str
+    name: str
+    ids: Dict[str, Union[str, int]]
+    cytogenic_location: Optional[str]
+    summary: Optional[str]
+    
+class AnnotatedGeneResponse(PagedResponseModel):
+    response: List[AnnotatedGene]
