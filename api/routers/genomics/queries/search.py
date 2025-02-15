9fcba336da7b7fb9d69a0e2013cc5048ed9c41ea
fossilfriend <egreenfest@gmail.com>
2025-02-15 11:42:13 -0500

    site (global) search queries - untested

--- api/routers/genomics/queries/search.py
+++ api/routers/genomics/queries/search.py
@@ -2,46 +2,61 @@ from typing import List
 from api.models.base_models import QueryDefinition
 from api.models.global_search import RecordSearchResult
 
-GENE_TEXT_SEARCH_SQL="SELECT * FROM gene_text_search((SELECT :term FROM st))"
-VARIANT_TEXT_SEARCH_SQL="SELECT * FROM variant_text_search((SELECT :term FROM st))"
-TRACK_TEXT_SEARCH_SQL="SELECT * FROM gwas_dataset_text_search((SELECT :term FROM st))"
-GLOBAL_SEARCH_SQL=(f'WITH Matches AS ({GENE_TEXT_SEARCH_SQL}'
-    f' UNION {VARIANT_TEXT_SEARCH_SQL} UNION {TRACK_TEXT_SEARCH_SQL}'
+__GENE_TEXT_SEARCH_SQL="SELECT * FROM gene_text_search((SELECT :keyword FROM st))"
+__VARIANT_TEXT_SEARCH_SQL="SELECT * FROM variant_text_search((SELECT :keyword FROM st))"
+__TRACK_TEXT_SEARCH_SQL="SELECT * FROM gwas_dataset_text_search((SELECT :keyword FROM st))"
+
+__GLOBAL_SEARCH_CTE=(f'WITH Matches AS ({__GENE_TEXT_SEARCH_SQL}'
+    f' UNION {__VARIANT_TEXT_SEARCH_SQL} UNION {__TRACK_TEXT_SEARCH_SQL}'
     f' ORDER BY match_rank, record_type, display ASC)'
-    f' SELECT jsonb_agg(matches)::text AS result FROM Matches'
 )
+
+__FEATURE_SEARCH_CTE=('WITH Matches AS ({__GENE_TEXT_SEARCH_SQL}'
+    f' UNION {__VARIANT_TEXT_SEARCH_SQL}'
+    f' ORDER BY match_rank, record_type, display ASC)'
+)
+
+
 
 # these are all fetchOne b/c the result is aggregated into a JSON object
 # TODO: test if SqlAlchemy can return JSON directly instead of returning the JSON as text and then parsing back
 
 GENE = QueryDefinition(
     name='gene-text-search',
-    query="SELECT jsonb_agg(*)::text AS result FROM (" + GENE_TEXT_SEARCH_SQL + ") m",
+    query="SELECT jsonb_agg(*)::text AS result FROM (" + __GENE_TEXT_SEARCH_SQL + ") m",
     resultType=List[RecordSearchResult],
-    bindParameters=['term'],
+    bindParameters=['keyword'],
     fetchOne=True
 )
 
 VARIANT = QueryDefinition(
     name='variant-text-search',
-    query="SELECT jsonb_agg(*)::text AS result FROM (" + VARIANT_TEXT_SEARCH_SQL + ") m",
+    query="SELECT jsonb_agg(*)::text AS result FROM (" + __VARIANT_TEXT_SEARCH_SQL + ") m",
     resultType=List[RecordSearchResult],
-    bindParameters=['term'],
+    bindParameters=['keyword'],
     fetchOne=True
 )
 
 TRACK = QueryDefinition(
     name='variant-text-search',
-    query="SELECT jsonb_agg(*)::text AS result FROM (" + TRACK_TEXT_SEARCH_SQL + ") m",
+    query="SELECT jsonb_agg(*)::text AS result FROM (" + __TRACK_TEXT_SEARCH_SQL + ") m",
     resultType=List[RecordSearchResult],
-    bindParameters=['term'],
+    bindParameters=['keyword'],
     fetchOne=True
 )
 
 GLOBAL = QueryDefinition(
     name='global-text-search',
-    query=GLOBAL_SEARCH_SQL,
+    query=f'{__GLOBAL_SEARCH_CTE} SELECT jsonb_agg(matches)::text AS result FROM Matches',
     resultType=List[RecordSearchResult],
-    bindParameters=['term', 'term', 'term'],
+    bindParameters=['keyword', 'keyword', 'keyword'],
+    fetchOne=True
+)
+
+FEATURE = QueryDefinition(
+    name='global-text-search',
+    query=f'{__FEATURE_SEARCH_CTE} SELECT jsonb_agg(matches)::text AS result FROM Matches',
+    resultType=List[RecordSearchResult],
+    bindParameters=['keyword', 'keyword'],
     fetchOne=True
 )
