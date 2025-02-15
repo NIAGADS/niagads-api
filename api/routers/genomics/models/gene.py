66dcfd3f808cda1f7b0a10512966d783d36ece9c
fossilfriend <egreenfest@gmail.com>
2025-02-15 15:41:02 -0500

    gene & variant model placeholders

--- api/routers/genomics/models/gene.py
+++ api/routers/genomics/models/gene.py
@@ -3,12 +3,20 @@ from typing import Dict, List, Optional, Union
 from api.models.base_response_models import PagedResponseModel
 from api.models.genome import Gene
 
-class AnnotatedGene(Gene):
+class AnnotatedGeneSummary(Gene):
     type: str
     name: str
     ids: Dict[str, Union[str, int]]
     cytogenic_location: Optional[str]
     summary: Optional[str]
+    
+class AnnotatedGene(AnnotatedGeneSummary):
+    function: Optional[dict]
+    pathways: Optional[dict]
+    relationships: Optional[dict]
+    
+class AnnotatedGeneSummaryResponse(PagedResponseModel):
+    response: List[AnnotatedGeneSummary]
     
 class AnnotatedGeneResponse(PagedResponseModel):
     response: List[AnnotatedGene]
--- api/routers/genomics/models/variant.py
+++ api/routers/genomics/models/variant.py
@@ -1,5 +1,31 @@
+from typing import Any, Dict, List, Optional, Union
 
+from pydantic import ConfigDict
+from api.models.base_response_models import PagedResponseModel
 from api.models.genome import Variant
 
-class AnnotatedVariant(Variant):
-    pass
+class AnnotatedVariantSummary(Variant):
+    type: str
+    is_adsp_variant: bool
+    most_serious_consequence: dict
+    cadd_score: Dict[str, float]
+    is_multi_allelic: bool
+
+class AnnotatedVariant(AnnotatedVariantSummary):
+    ADSP_qc: Optional[Dict[str, Union[str, int, bool]]]
+    allele_frequencies: Optional[dict]
+    # predicted consequences
+    # lof
+    # favor annotations
+    # ld -> maybe?
+    
+
+    # FIXME: not sure on this
+    # model_config = ConfigDict(exclude_none=True)
+
+
+class AnnotatedVariantSummaryResponse(PagedResponseModel):
+    response: List[AnnotatedVariantSummary]
+    
+class AnnotatedVariantResponse(PagedResponseModel):
+    response: List[AnnotatedVariant]
