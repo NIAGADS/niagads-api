11bee96ae3a4d60f947ca324a7328dbf1fa53793
fossilfriend <egreenfest@gmail.com>
2025-02-16 01:15:39 -0500

    fix search definition

--- api/routers/genomics/queries/search.py
+++ api/routers/genomics/queries/search.py
@@ -1,62 +1,65 @@
+from enum import auto
 from typing import List
+
+from api.common.enums import EnumParameter
 from api.models.base_models import QueryDefinition
 from api.models.search import RecordSearchResult
 
-__GENE_TEXT_SEARCH_SQL="SELECT * FROM gene_text_search((SELECT :keyword FROM st))"
-__VARIANT_TEXT_SEARCH_SQL="SELECT * FROM variant_text_search((SELECT :keyword FROM st))"
-__TRACK_TEXT_SEARCH_SQL="SELECT * FROM gwas_dataset_text_search((SELECT :keyword FROM st))"
-
-__GLOBAL_SEARCH_CTE=(f'WITH Matches AS ({__GENE_TEXT_SEARCH_SQL}'
-    f' UNION {__VARIANT_TEXT_SEARCH_SQL} UNION {__TRACK_TEXT_SEARCH_SQL}'
-    f' ORDER BY match_rank, record_type, display ASC)'
-)
-
-__FEATURE_SEARCH_CTE=('WITH Matches AS ({__GENE_TEXT_SEARCH_SQL}'
-    f' UNION {__VARIANT_TEXT_SEARCH_SQL}'
-    f' ORDER BY match_rank, record_type, display ASC)'
-)
 
 
+class SearchType(EnumParameter): # TODO: move to parameters
+    GENE = auto()
+    VARIANT = auto()
+    FEATURE = auto()
+    TRACK = auto()
+    GLOBAL = auto()
+    
+    @classmethod
+    def get_description(cls, inclValues=True):
+        message = 'Type of site search to perform.'    
+        return message + f' {super().get_description()}' if inclValues else message
 
 # these are all fetchOne b/c the result is aggregated into a JSON object
 # TODO: test if SqlAlchemy can return JSON directly instead of returning the JSON as text and then parsing back
 
-GENE = QueryDefinition(
-    name='gene-text-search',
-    query="SELECT jsonb_agg(*)::text AS result FROM (" + __GENE_TEXT_SEARCH_SQL + ") m",
-    resultType=List[RecordSearchResult],
-    bindParameters=['keyword'],
-    fetchOne=True
-)
+class SiteSearchQueryDefinition(QueryDefinition):
+    searchType: SearchType
+    resultType: List[RecordSearchResult]   
+    fetchOne: bool = True
+    
+    def __get_cte(self):
+        
+        geneSql = "SELECT * FROM gene_text_search((SELECT :keyword FROM st))"
+        variantSql = "SELECT * FROM variant_text_search((SELECT :keyword FROM st))"
+        trackSql = "SELECT * FROM gwas_dataset_text_search((SELECT :keyword FROM st))"
+        
+        match self.searchType:
+            case SearchType.GENE:
+                return geneSql
+            case SearchType.VARIANT:
+                return variantSql
+            case SearchType.TRACK:
+                return trackSql
+            case SearchType.FEATURE:
+                return ('WITH Matches AS ({geneSql}'
+                    f' UNION {variantSql}'
+                    f' ORDER BY match_rank, record_type, display ASC)'
+                )
+            case _:
+                return (f'WITH Matches AS ({geneSql}'
+                    f' UNION {variantSql} UNION {trackSql}'
+                    f' ORDER BY match_rank, record_type, display ASC)'
+                )
 
-VARIANT = QueryDefinition(
-    name='variant-text-search',
-    query="SELECT jsonb_agg(*)::text AS result FROM (" + __VARIANT_TEXT_SEARCH_SQL + ") m",
-    resultType=List[RecordSearchResult],
-    bindParameters=['keyword'],
-    fetchOne=True
-)
-
-TRACK = QueryDefinition(
-    name='variant-text-search',
-    query="SELECT jsonb_agg(*)::text AS result FROM (" + __TRACK_TEXT_SEARCH_SQL + ") m",
-    resultType=List[RecordSearchResult],
-    bindParameters=['keyword'],
-    fetchOne=True
-)
-
-GLOBAL = QueryDefinition(
-    name='global-text-search',
-    query=f'{__GLOBAL_SEARCH_CTE} SELECT jsonb_agg(matches)::text AS result FROM Matches',
-    resultType=List[RecordSearchResult],
-    bindParameters=['keyword', 'keyword', 'keyword'],
-    fetchOne=True
-)
-
-FEATURE = QueryDefinition(
-    name='global-text-search',
-    query=f'{__FEATURE_SEARCH_CTE} SELECT jsonb_agg(matches)::text AS result FROM Matches',
-    resultType=List[RecordSearchResult],
-    bindParameters=['keyword', 'keyword'],
-    fetchOne=True
-)
+    
+    def model_post_init(self, __context):        
+        match self.searchType:
+            case SearchType.FEATURE:
+                self.bindParameters = ['keyword', 'keyword']
+            case SearchType.GLOBAL:
+                self.bindParameters = ['keyword', 'keyword', 'keyword']
+            case _:
+                self.bindParameters = ['keyword']
+        
+        self.name = f'{self.searchType.value}-text-search'
+        self.query = f'SELECT jsonb_agg(*)::text AS result FROM ({self.__get_cte()}) m'
