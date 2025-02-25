6c6225d99e5708c22f823c11e20988ad157c75b4
fossilfriend <egreenfest@gmail.com>
2025-02-25 05:09:27 +0000

    comment - alt command

--- start.sh
+++ start.sh
@@ -15,4 +15,6 @@ echo "Build: $BUILD_ENV / Log Level: ${LOG_LEVEL}"
 # Start Uvicorn with live reload
 # --host 0.0.0.0 tells uvicorn to ignore host machine IP
 exec uvicorn --app-dir /app --host 0.0.0.0 --port 8000 --reload --reload-dir /app/api --log-level $LOG_LEVEL "$APP_MODULE" 
+# fastapi run --workers 4 api/main.py
+
 
