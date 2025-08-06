75e2ffd528919820c8933fe97f3f590d1c554b57
fossilfriend <egreenfest@gmail.com>
2025-08-06 18:45:52 -0400

    clean up fetching and only keep handler

--- docs/lib/route-handlers.ts
+++ docs/lib/route-handlers.ts
@@ -1,80 +1,12 @@
-// FIXME: most of this is in @niagads/common - after published
-import * as path from "path";
 import { NextRequest, NextResponse } from "next/server";
-
-export function userAgentIsBrowser(userAgent: string): boolean {
-	// checks if the user-agent for the request was a web browser
-	const browsers = ["Mozilla", "Safari", "Edge", "Chrome"];
-	return (
-		userAgent !== null &&
-		browsers.some((value) => userAgent.includes(value))
-	);
-}
-
-/**
- *
- * @param requestUri
- * @param asText - if 'true' returns text response else returns jso response
- * @param skipErrorCheck  - defaults to 'true' b/c NIAGADS API calls will handle errors and return a JSON message
- * @returns - the response
- */
-async function __fetch(
-	requestUri: string,
-	asText: boolean = false,
-	skipErrorCheck: boolean = true
-) {
-	const response = await fetch(requestUri);
-	if (!response.ok) {
-		if (skipErrorCheck) {
-			const error = await response.json();
-			return Object.assign({}, error, { status: response.status });
-		} else {
-			throw new Error(
-				`Fetch from ${requestUri} failed with status ${response.status}: ${response.statusText}`
-			);
-		}
-	}
-	const data = asText ? await response.text() : await response.json();
-	return data;
-}
-
-export async function backendFetchFromRequest(
-	request: NextRequest,
-	base: string,
-	asText = false
-) {
-	const incomingRequestUrl = new URL(request.url);
-	const pathname = incomingRequestUrl.pathname;
-	const queryParams = incomingRequestUrl.search;
-	const baseUrl = base.startsWith("http")
-		? base
-		: new URL(base, incomingRequestUrl.origin).toString();
-	const requestUri: string = new URL(
-		path.join(pathname, queryParams),
-		baseUrl
-	).toString();
-	return await __fetch(requestUri, asText);
-}
-
-export async function backendFetch(
-	pathname: string,
-	base: string | undefined,
-	skipErrorCheck: boolean = true
-) {
-	const requestUri: string = base
-		? new URL(pathname, base).toString()
-		: new URL(pathname).toString();
-	return await __fetch(requestUri, false, skipErrorCheck);
-}
+import {
+	backendFetchFromRequest,
+	caseInsensitiveIncludes,
+} from "@niagads/common";
 
 const TEXT_RESPONSES: string[] = ["TEXT", "VCF", "BED"];
 
-export function caseInsensitiveIncludes(array: string[], value: string) {
-	const lcValue = value.toLowerCase();
-	return array.some((item) => item.toLowerCase().includes(lcValue));
-}
-
-export async function backendFetchResponseHandler(
+export async function __apiFetch(
 	request: NextRequest,
 	headers: any | undefined = null
 ) {
@@ -100,9 +32,13 @@ export async function backendFetchResponseHandler(
 		if (!caseInsensitiveIncludes(["DEFAULT"], queryParams["view"])) {
 			if (caseInsensitiveIncludes(["TABLE"], queryParams["view"])) {
 				// redirect to the correct view, passing the original query as an arg
-				const redirectEndpoint = `/view/table?endpoint=${
+				const encodedQuery = btoa(
+					`${incomingRequestUrl.pathname}${incomingRequestUrl.search}`
+				);
+				/* const redirectEndpoint = `/view/table?endpoint=${
 					incomingRequestUrl.pathname
-				}/${incomingRequestUrl.search.replace("?", "&")}`;
+				}/${incomingRequestUrl.search.replace("?", "&")}`; */
+				const redirectEndpoint = `/view/table?query=${encodedQuery}`;
 				const redirectUrl = new URL(
 					redirectEndpoint,
 					process.env.NEXT_PUBLIC_HOST_URL
