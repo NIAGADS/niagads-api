2682a1c9ddf73b4e3c50f1eb181cf3cf8a0570b4
fossilfriend <egreenfest@gmail.com>
2025-08-06 19:17:13 -0400

    navbar bakground white

--- docs/components/navbar.tsx
+++ docs/components/navbar.tsx
@@ -31,7 +31,7 @@ const algolia_props = {
 
 export function Navbar() {
 	return (
-		<nav className="w-full border-b h-16 sticky top-0 z-50 bg-background">
+		<nav className="w-full border-b h-16 sticky top-0 z-50 bg-white">
 			<div className="sm:container mx-auto w-[95vw] h-full flex items-center sm:justify-between md:gap-2">
 				<div className="flex items-center sm:gap-5 gap-2.5">
 					<SheetLeftbar />
